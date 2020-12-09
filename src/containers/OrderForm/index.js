24f239c3055c8e9f5da51c849968581f66d3ed19
Huannt <huannt@bestarion.com>
2020-12-09 10:32:55 +0700

    Fix bug

--- src/containers/OrderForm/index.js
+++ src/containers/OrderForm/index.js
@@ -48,7 +48,7 @@ class OrderForm extends Component {
       newOrder.toolId = orderEditting.toolId
       newOrder.userId = orderEditting.userId
       newOrder.status = orderEditting.status
-      if (user.admin || newOrder.userId === user._id) {
+      if (user.admin || newOrder.userId._id === user._id) {
         updateOrder(newOrder);
       }
     } else {
